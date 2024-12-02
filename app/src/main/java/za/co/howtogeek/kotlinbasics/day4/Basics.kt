d4518bcf3abf222b8cc6eb2601158466d5c25bd1
DaHub49 <dyls.martin.49@gmail.com>
2024-12-02 06:42:23 +0200

    End of video 65 -> day 4.

--- app/src/main/java/za/co/howtogeek/kotlinbasics/day4/Basics.kt
+++ app/src/main/java/za/co/howtogeek/kotlinbasics/day4/Basics.kt
@@ -6,6 +6,19 @@ fun main() {
     //val shoppingList = listOf("Processor", "RAM", "Graphics Card", "SSD")
 
     // Mutable List (can be modified):
-    val shoppingList = mutableListOf("Processor", "RAM", "Graphics Card", "SSD")
+    val shoppingList = mutableListOf("Processor", "RAM", "Graphics Card RTX 3060", "SSD")
+
+    // Adding items to list:
+    shoppingList.add("Cooling System")
+    shoppingList.remove("Graphics Card RTX 3060")
+    shoppingList.add("Graphics Card RTX 4090")
+
+    println(shoppingList)
+    shoppingList.removeAt(2)
+    println(shoppingList)
+    shoppingList.removeAt(1)
+    println(shoppingList)
+    shoppingList.add(2, "RAM")
+    println(shoppingList)
 
 }
