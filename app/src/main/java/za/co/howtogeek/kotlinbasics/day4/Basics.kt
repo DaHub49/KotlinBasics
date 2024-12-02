593d41f9d6d2810941b64cafc639f57c2e5d512b
DaHub49 <dyls.martin.49@gmail.com>
2024-12-02 08:04:33 +0200

    End of video 66 -> day 4.

--- app/src/main/java/za/co/howtogeek/kotlinbasics/day4/Basics.kt
+++ app/src/main/java/za/co/howtogeek/kotlinbasics/day4/Basics.kt
@@ -7,18 +7,43 @@ fun main() {
 
     // Mutable List (can be modified):
     val shoppingList = mutableListOf("Processor", "RAM", "Graphics Card RTX 3060", "SSD")
+    println(shoppingList)
+    println("shoppingList.size: ${shoppingList.size}\n")
 
     // Adding items to list:
     shoppingList.add("Cooling System")
     shoppingList.remove("Graphics Card RTX 3060")
     shoppingList.add("Graphics Card RTX 4090")
+    println(shoppingList)
+    println("shoppingList.size: ${shoppingList.size}\n")
 
-    println(shoppingList)
+    println("\nUpdated Shopping List:")
+    println("shoppingList[0]: ${shoppingList[0]}, " +
+            "shoppingList[1]: ${shoppingList[1]}, " +
+            "shoppingList[2]: ${shoppingList[2]}, " +
+            "shoppingList[3]: ${shoppingList[3]}, " +
+            "shoppingList[4]: ${shoppingList[4]}\n")
+    println("remove at (2)")
     shoppingList.removeAt(2)
-    println(shoppingList)
+    println("shoppingList.size: ${shoppingList.size}")
+
+    println("shoppingList: $shoppingList\n")
+    println("remove at (1)")
     shoppingList.removeAt(1)
-    println(shoppingList)
+    println("shoppingList.size: ${shoppingList.size}")
+
+    println("shoppingList.add(2), RAM")
     shoppingList.add(2, "RAM")
-    println(shoppingList)
+    println("shoppingList[2]: ${shoppingList[2]}")
+    //Shifts list elements to the right:
+    println("shoppingList[3]: ${shoppingList[3]}")
+
+    // update field:
+    shoppingList[3] = "Graphics Card RX 7800XT"
+    println("\nUpdated Shopping List:")
+    println("shoppingList[0]: ${shoppingList[0]}, " +
+            "shoppingList[1]: ${shoppingList[1]}, " +
+            "shoppingList[2]: ${shoppingList[2]}, " +
+            "shoppingList[3]: ${shoppingList[3]}\n")
 
 }
